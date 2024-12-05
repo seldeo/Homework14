5ee34b50ac3be31adc06566b1f118630d33f22d3
unknown <selinaadwoa1@gmail.com>
2024-12-05 12:29:44 -0800

    adding all folder

---
+++ Homework14/src/index.js
@@ -0,0 +1,39 @@
+let title = document.getElementById("title");
+title.style.backgroundColor = "blue";
+
+
+//Using Js to style my first paragraph
+let DOM = document.getElementById("DOM");
+DOM.style.backgroundColor = "gray";
+DOM.style.color = "black"
+DOM.innerHTML = "I know now what the DOM is 🤲 "
+
+
+//Using Js to style my Seconf Paragraph
+let sel = document.getElementById("sel");
+sel.style.backgroundColor = "Yellow";
+sel.style.color = "black";
+sel.innerHTML = "I know how to access/select elements in the DOM 👊";
+
+
+//Using Js to style my third paragraph
+let use = document.getElementById("use");
+use.style.backgroundColor = "green";
+use.style.color = "black";
+use.innerHTML = "I know how to use the style methods to change the style of an element in the DOM ✅";
+
+
+//Using Js to style my fourth paragraph
+let styl = document.getElementById("styl");
+styl.style.backgroundColor = "purple";
+styl.style.color = "white";
+styl.innerHTML = "I know how the styling properties differ in JS from CSS👍";
+
+
+//using Js to style my fifth paragraph
+let excited = document.getElementById("excited");
+excited.style.backgroundColor = "black";
+excited.style.color = "white"
+excited.innerText = "Yes I did it!! I manipulated the DOM and styled my elements 🥰🥰🥰🥰"
+
+
